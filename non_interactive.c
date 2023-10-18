e1ceaeac3bf88c7da6543047e03a1d0754c93ed6
Muhammad Omran <use.yurMind@gmail.com>
2023-10-18 08:51:39 -0700

    implement non interactive func

---
+++ non_interactive.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * non_interactive - fn
+ *
+ * Return: void
+ */
+
+void non_interactive(void)
+{
+	char **cur_cmd = NULL;
+	int i, cmd_type = 0;
+	size_t n = 0;
+
+	if (!(isatty(STDIN_FILENO)))
+	{
+		while (getline(&lines, &n, stdin) != -1)
+		{
+			remove_newline(lines);
+			remove_comment(lines);
+			cmds = tokenizer(lines, ";");
+			for (i = 0; cmds[i] != NULL; i++)
+			{
+				cur_cmd = tokenizer(cmds[i], " ");
+				if (cur_cmd[0] == NULL)
+				{
+					free(cur_cmd);
+					break;
+				}
+				cmd_type = parse_command(cur_cmd[0]);
+				initializer(cur_cmd, cmd_type);
+				free(cur_cmd);
+			}
+			free(cmds);
+		}
+		free(lines);
+		exit(status);
+	}
+}
